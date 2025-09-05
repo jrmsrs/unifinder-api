a20ad4496ac9489710cf64e31ab12b7903edd74d
Junior M. Soares <jrmsrs2.0@gmail.com>
2025-09-05 12:39:05 -0300

    add pg support and update database configuration

--- app/infra/database.py
+++ app/infra/database.py
@@ -1,7 +1,11 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlmodel import Session
 
-DATABASE_URL = "sqlite:///./database.db"
+load_dotenv()
+# DATABASE_URL = "sqlite:///./database.db"
+DATABASE_URL = os.getenv("DATABASE_URL") or "sqlite:///./database.db"
 
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 # SessionLocal = sessionmaker(bind=engine, autoflush=False)
