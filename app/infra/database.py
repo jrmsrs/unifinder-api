14972dc52f96512df69240916c75ac656b0e9d2f
Jordana Cavalcante <jordana-cavalcante@outlook.com>
2025-11-03 12:55:58 -0300

    fix database pool

--- app/infra/database.py
+++ app/infra/database.py
@@ -1,17 +1,19 @@
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlmodel import Session
 from app.infra.config import Settings
 
-# load_dotenv()
-# DATABASE_URL = "sqlite:///./database.db"
 DATABASE_URL = Settings.database_url
 
-# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-# SessionLocal = sessionmaker(bind=engine, autoflush=False)
-# Base = declarative_base()
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(
+    DATABASE_URL,
+    echo=False,
+    pool_size=5,
+    max_overflow=2,
+    pool_timeout=30,
+    pool_recycle=1800,
+    pool_pre_ping=True
+)
 
 def get_session():
     with Session(engine) as session:
